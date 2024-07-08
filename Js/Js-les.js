9ee04ad4bc01140a0747653d90a12a2b9167c3d5
osenoah <noahoshose2@gmail.com>
2024-07-08 09:22:39 +0100

    update

--- Js/Js-les.js
+++ Js/Js-les.js
@@ -1,7 +1,7 @@
 // variables 
 //as a container that has a value
-
-    var x = 10;
-    var y = 27;
-    var z = x + y;
-
+var elem = document.getElementById("dem");
+var x = 10;
+var y = 27;
+var z = (x * y) / (x + y);
+document.getElementById("dem").innerHTML = z;
