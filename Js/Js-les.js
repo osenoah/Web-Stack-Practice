4a3c43548afc86e859ac132f90f01ef8d83bfd62
osenoah <noahoshose2@gmail.com>
2024-07-08 11:42:41 +0100

    update

--- Js/Js-les.js
+++ Js/Js-les.js
@@ -1,7 +1,20 @@
-// variables 
-//as a container that has a value
-var elem = document.getElementById("dem");
-var x = 10;
-var y = 27;
-var z = (x * y) / (x + y);
-document.getElementById("dem").innerHTML = z;
+
+ //calling the id on html
+ //int JS
+ //arith operations
+    var a = 10;
+    a--;
+    var b = 27;
+    b++;
+    var c = 24;
+    var x = (a * b) / (a + b);
+    var y = x * c;
+    var z = b;                    
+
+    var me = {firstname:"Ose", lastname:"Noah", age:c, eyecolor:"Coffee Brown", religion:"Christian", gender:"male"}
+                
+    document.getElementById("dem").innerHTML = y;
+    document.getElementById("ari").innerHTML = x;
+    document.getElementById("ops").innerHTML = z;
+    document.getElementById('me').innerHTML = 'My initial is '+ me.lastname +" "+ me.firstname +'.'+" "+'I am'+" "+ me.age +" "+ 'years old '+'.'+" "+ 'I am a' +"&nbsp"+ me.religion;
+                                    
