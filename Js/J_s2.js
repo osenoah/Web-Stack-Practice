7625270e390ba1a4481706643c9e7f74cc7c2989
osenoah <noahoshose2@gmail.com>
2024-07-12 22:35:18 +0100

    update

--- Js/J_s2.js
+++ Js/J_s2.js
@@ -1,2 +1,11 @@
-console.log('Hello, DevTools!');
-alert('Hello World');
+// alert('Hello World');
+var a = 10;
+var b = 20;
+if ( b > a ) {
+    console.log('b is greater than a');
+    return 1;
+}
+function say() {
+    var msg = "Hi";
+    return msg;
+}
