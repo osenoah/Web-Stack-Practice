f4cdb9817256696078a426c4fd1c68f12f23b812
osenoah <noahoshose2@gmail.com>
2024-07-13 06:58:57 +0100

    update

--- Js/J_s2.js
+++ Js/J_s2.js
@@ -1,10 +1,15 @@
-// alert('Hello World');
+"use strict";
+
+alert('Hello World');
+ 
 var a = 10;
 var b = 20;
+
 if ( b > a ) {
     console.log('b is greater than a');
     return 1;
 }
+
 function say() {
     var msg = "Hi";
     return msg;
