e8d1436a93873c84ec5d506bc0efae47cbebcbaf
osenoah <noahoshose2@gmail.com>
2024-07-17 05:30:01 +0100

    update

---
+++ Js/index.js
@@ -0,0 +1,38 @@
+/* 
+function increment() {
+    console.log("It works")
+ }
+ increment()
+
+ function JJ() {
+    console.log(42)
+ }
+ JJ()
+
+ let lap1 = 23
+ let lap2 = 39
+ let lap3 = 45
+
+ function Laps() {
+    console.log(lap1 + lap2 + lap3)
+ }
+
+ Laps()
+
+ let dog = 0
+
+ function Dogs() {
+    dog = dog + 1
+}
+ Dogs()
+ Dogs()
+ Dogs()
+ console.log(dog) */
+
+ let count = 0
+
+function Yeat() {
+    count = count + 1
+    console.log(count)
+}
+Yeat()
